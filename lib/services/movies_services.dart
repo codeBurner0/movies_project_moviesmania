c38980e7770ee1052f9640e0f242564edd902228
neha <neha309shar@gmail.com>
2022-07-07 20:53:26 +0530

    changes

--- lib/services/movies_services.dart
+++ lib/services/movies_services.dart
@@ -32,7 +32,40 @@ class MovieService {
       }).toList();
       return _movies;
     }else{
-      throw Exception('');
+      throw Exception('Couldn\'t load popular movies.');
+    }
+  }
+
+  Future<List<Movie>> getUpcomingMovies({int page}) async{
+    Response _response = await _http.get('/movie/upcoming', query: {
+      'page': page,
+    });
+    
+    if(_response.statusCode == 200){
+      Map _data =_response.data;
+      List<Movie> _movies = _data['results'].map<Movie>((_movieData)  {
+        return Movie.fromJson(_movieData);
+      }).toList();
+      return _movies;
+    }else{
+      throw Exception('Couldn\'t load upcoming movies.');
+    }
+  }
+
+  Future<List<Movie>> searchMovies(String _searchTerm,{int page}) async{
+    Response _response = await _http.get('/search/movie', query: {
+      'query' : _searchTerm,
+      'page': page,
+    });
+    
+    if(_response.statusCode == 200){
+      Map _data =_response.data;
+      List<Movie> _movies = _data['results'].map<Movie>((_movieData)  {
+        return Movie.fromJson(_movieData);
+      }).toList();
+      return _movies;
+    }else{
+      throw Exception('Couldn\'t perform movies search.');
     }
   }
 
